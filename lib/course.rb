afd9ece67c88c38c83702fbfbaff8fa1b4133b3b
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-07 14:04:22 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,11 +2,15 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
-  def self.all
-    @@all
+  def initialize
+    @@all << self
   end 
 
+  def self.all
+    @@all
+  end
+
   def self.reset_all
-    @@all.clear 
-  end 
+    @@all.clear
+  end
 end
